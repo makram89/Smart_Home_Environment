06846be3e54b42daca685ce2dd88a24e91fd2002
makram89 <34419130+makram89@users.noreply.github.com>
2020-03-01 21:46:17 +0100

    everything working

--- src/src.ino
+++ src/src.ino
@@ -1,4 +1,3 @@
-
 #include "dht.h"
 #include "DFRobot_BMP388.h"
 #include "DFRobot_BMP388_I2C.h"
@@ -21,22 +20,22 @@
 #define OLED_CS    10
 #define SPI_MOSI   11    /* connect to the DIN pin of OLED */
 #define SPI_SCK    13     /* connect to the CLK pin of OLED */
- 
+
 dht DHT22;
- 
+
 #define DHT22PIN 4    //przypisanie pinu 2 Arduino jako odczyt z sensora
 DFRobot_BMP388_I2C bmp388;
 
 uint8_t oled_buf[WIDTH * HEIGHT / 8];
-void setup()
-{
+
+void setup() {
 
     Serial.begin(9600);                    //inicjalizacja monitora szeregowego
 
 //BMP
     bmp388.set_iic_addr(BMP3_I2C_ADDR_SEC);
     /* Initialize bmp388*/
-    while(bmp388.begin()){
+    while (bmp388.begin()) {
         Serial.println("Initialize error!");
         delay(1000);
     }
@@ -44,28 +43,27 @@ void setup()
     SH1106_begin();
 
 
-    Serial.println("collecting data - started"); 
+    Serial.println("collecting data - started");
 }
- 
-void loop()
-{
-    int chk = DHT22.read(DHT22PIN);         //sprawdzenie stanu sensora, a następnie wyświetlenie komunikatu na monitorze szeregowym
+
+void loop() {
+    int chk = DHT22.read(
+            DHT22PIN);         //sprawdzenie stanu sensora, a następnie wyświetlenie komunikatu na monitorze szeregowym
 
 //    TODO powiadomienie o stanie sesora
     Serial.print("Stan sensora: ");
-    switch (chk)
-    {
-        case DHTLIB_OK: 
-            Serial.print("OK\t"); 
+    switch (chk) {
+        case DHTLIB_OK:
+            Serial.print("OK\t");
             break;
-        case DHTLIB_ERROR_CHECKSUM: 
-            Serial.println("Błąd sumy kontrolnej"); 
+        case DHTLIB_ERROR_CHECKSUM:
+            Serial.println("Błąd sumy kontrolnej");
             break;
-        case DHTLIB_ERROR_TIMEOUT: 
-            Serial.println("Koniec czasu oczekiwania - brak odpowiedzi"); 
+        case DHTLIB_ERROR_TIMEOUT:
+            Serial.println("Koniec czasu oczekiwania - brak odpowiedzi");
             break;
-        default: 
-            Serial.println("Nieznany błąd"); 
+        default:
+            Serial.println("Nieznany błąd");
             break;
     }
 
@@ -77,51 +75,44 @@ void loop()
 //    Serial.print("Temperatura (C): ");           //wyświetlenie temperatury
 //    Serial.println((float)DHT22.temperature, 2);
 
-    float humidity = (float)DHT22.humidity;
-    
 
-    
 
-    //   Barometr
-    float Pressure = bmp388.readPressure();
-//    Serial.print("Pressure(hPa): ");
-//    Serial.print(Pressure/100);
-//    Serial.print("\n");
-//    delay(100);
-    /* Read the temperature, print data via serial port.*/
-    float Temperature = bmp388.readTemperature();
-//    Serial.print("Temperature (C) : ");
-//    Serial.print(Temperature);
-//    Serial.print("\n");
+
+
+    float temperature_f = DHT22.temperature;
+    char temperature[5];
+    dtostrf(temperature_f, 4, 1, temperature);
+
+
+    float pressure_f = bmp388.readPressure() / 100;
+    char pressure[7];
+    dtostrf(pressure_f, 6, 2, pressure);
+
+    float humidity_f = DHT22.humidity;
+    char humidity[4];
+    dtostrf(humidity_f, 3, 1, humidity);
 
 //    int seconds = Wire.read(); // get seconds
 //    int minutes = Wire.read(); // get minutes
 //    int hours = Wire.read();   // get hours
 
     SH1106_clear(oled_buf);
-// Head
-    SH1106_string(0, 0, "Temp", 12, 1, oled_buf);
-    SH1106_string(46, 0, "P[hPa]", 12, 1, oled_buf);
-    SH1106_string(90, 0, "Hum[%]", 12, 1, oled_buf);
+// Head 1st col
+    SH1106_string(0, 16, "Temp:", 16, 1, oled_buf);
+    SH1106_string(0, 32, "Pres:", 16, 1, oled_buf);
+    SH1106_string(0, 48, "Hum:", 16, 1, oled_buf);
 
-// Dashes
-    SH1106_char(40, 16, '|',12,1, oled_buf);
-    SH1106_char(80, 16, '|',12,1, oled_buf);
-    SH1106_char(40, 32, '|',12,1, oled_buf);
-    SH1106_char(80, 32, '|',12,1, oled_buf);
-    SH1106_char(40, 48, '|',12,1, oled_buf);
-    SH1106_char(80, 48, '|',12,1, oled_buf);
 
 // Values
-    char temperature[5] ;
-    dtostrf(34.9,4,1,temperature);
+    SH1106_string(50, 16, temperature, 16, 1, oled_buf);
+    SH1106_string(112, 16, "C", 16, 1, oled_buf);
+    SH1106_string(106, 14, "o", 12, 1, oled_buf);
 
-    char pressure[5];
-    dtostrf(34.9,4,1,temperature);
-    SH1106_string(0, 16, temperature, 16, 1, oled_buf);
-    SH1106_string(8, 32, "C*", 16, 1, oled_buf);
-    SH1106_string(46, 32, pressure, 12, 1, oled_buf);
-    SH1106_string(8, 32, "C*", 16, 1, oled_buf);
+    SH1106_string(50, 32, pressure, 16, 1, oled_buf);
+    SH1106_string(106, 34, "hPa", 12, 1, oled_buf);
+
+    SH1106_string(50, 48, humidity, 16, 1, oled_buf);
+    SH1106_string(106, 48, "%", 16, 1, oled_buf);
 
     SH1106_display(oled_buf);
 
